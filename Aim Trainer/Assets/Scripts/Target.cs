f5497f823077487d951b3c74eedea5c77491b347
Jessica Hernández Gómez <jessicahg04@gmail.com>
2021-08-12 12:46:42 +0200

    Birds targets

--- Aim Trainer/Assets/Scripts/Target.cs
+++ Aim Trainer/Assets/Scripts/Target.cs
@@ -4,8 +4,12 @@ using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 public class Target : MonoBehaviour {
-    public GameObject circle;
-    public Rigidbody2D rb;
+    [SerializeField] private GameObject _circle;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private CircleCollider2D _collider;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite _targetSprite;
+    [SerializeField] private Sprite _birdSprite;
     private Vector2 _direction = new Vector2(0, 0);
     private string _currentSceneName;
     private float _speed;
@@ -18,6 +22,14 @@ public class Target : MonoBehaviour {
             _direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
         }
         _currentSceneName = SceneManager.GetActiveScene().name;
+        if(_currentSceneName == "Motionless Target Scene"){
+            _spriteRenderer.sprite = _targetSprite;
+            _collider.radius = 0.61f;
+        }else{
+            _spriteRenderer.sprite = _birdSprite;
+            _collider.radius = 0.47f;
+            _collider.offset = new Vector2(-0.04f, 0.0f);
+        }
         if(_currentSceneName == "Motionless Target Scene" || _currentSceneName == "Moving Target Scene") {
             Destroy(gameObject, GameManager.GetInstance().GetDestroyTime());
         }
@@ -26,7 +38,8 @@ public class Target : MonoBehaviour {
     private void Update() {
         if(_currentSceneName == "Moving Target Scene") {
             CheckTargetOutScreen();
-            rb.velocity = _direction * _speed;
+            CheckSpriteLookAt();
+            _rigidbody.velocity = _direction * _speed;
         }
 
         if(_currentSceneName == "Target Tracking Scene") {
@@ -39,21 +52,42 @@ public class Target : MonoBehaviour {
                 }
             }
             CheckTargetOutScreen();
-            rb.velocity = _direction * _speed;
+            CheckSpriteLookAt();
+            _rigidbody.velocity = _direction * _speed;
         }
     }
 
     private void CheckTargetOutScreen() {
-        if(Mathf.Round(transform.position.x) <= -84f){ //Left
+        if(Mathf.Round(transform.position.x) <= -82){ //Left
             _direction.x = 1f;
-        }else if(Mathf.Round(transform.position.x) >= 84f){ //Right
+        }else if(Mathf.Round(transform.position.x) >= 82f){ //Right
             _direction.x = -1f;
         }
 
-        if(Mathf.Round(transform.position.y) >= 39){ //Up
+        if(Mathf.Round(transform.position.y) >= 38){ //Up
             _direction.y = -1f;
-        }else if(Mathf.Round(transform.position.y) <= -45){ //Down
+        }else if(Mathf.Round(transform.position.y) <= -27){ //Down
             _direction.y = 1f;
+        }
+    }
+
+    private void CheckSpriteLookAt() {
+        if(_direction.x == -1f && _direction.y == 1f){ //Left and up
+            transform.rotation = Quaternion.Euler(0, -180, 45);
+        }else if(_direction.x == -1f && _direction.y == 0f){ //Left
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }else if(_direction.x == -1f && _direction.y == -1f){ //Left and down
+            transform.rotation = Quaternion.Euler(0, -180, -45);
+        }else if(_direction.x == 0f && _direction.y == 1f){ //Up
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }else if(_direction.x == 0f && _direction.y == -1f){ //Down
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }else if(_direction.x == 1f && _direction.y == 1f){ //Right and up
+            transform.rotation = Quaternion.Euler(0, 0, 45);
+        }else if(_direction.x == 1f && _direction.y == 0f){ //Right
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }else if(_direction.x == 1f && _direction.y == -1f){ //Right and down  
+            transform.rotation = Quaternion.Euler(0, 0, -45);
         }
     }
 
@@ -66,9 +100,13 @@ public class Target : MonoBehaviour {
         int score = maxScore - (scoreMultiplier * (int)distance);
 
         GameManager.GetInstance().UpdateScore(score);
-        var go = Instantiate(circle, transform.position, Quaternion.identity);
-        var text = go.GetComponentInChildren<TextMesh>();
-        text.text = score.ToString();
+        
+        if(_currentSceneName != "Target Tracking Scene") {
+            var go = Instantiate(_circle, transform.position, Quaternion.identity);
+            var text = go.GetComponentInChildren<TextMesh>();
+            text.text = score.ToString();
+        }
+        
         if(_currentSceneName == "Motionless Target Scene" || _currentSceneName == "Moving Target Scene") {
             Destroy(gameObject);
         }
